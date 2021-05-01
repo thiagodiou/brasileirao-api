caedf56f7213ffe8cc84cdd61e304c64d8a5dd8a
Thiago Araujo <thiagodiou@gmail.com>
2021-05-01 11:53:53 -0300

    Subindo primeira versao do projeto

---
+++ src/test/java/br/com/thiagodiou/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagodiou.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
