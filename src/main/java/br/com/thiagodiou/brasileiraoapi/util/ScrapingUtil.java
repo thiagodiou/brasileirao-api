f4ec7c2e2b69813fef6ded8712a5487708886ccc
Thiago Araujo <thiagodiou@gmail.com>
2021-05-01 17:18:17 -0300

    Conectando jsoup

--- src/main/java/br/com/thiagodiou/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/thiagodiou/brasileiraoapi/util/ScrapingUtil.java
@@ -1,15 +1,40 @@
 package br.com.thiagodiou.brasileiraoapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.thiagodiou.brasileiraoapi.dto.PartidaGoogleDTO;
 
 public class ScrapingUtil {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+
 	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
-	
+
 	public static void main(String[] args) {
-	
-	
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
 	}
 
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+
+		Document document = null;
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			LOGGER.info("Titulo da pagina: {}", title);
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM O JSOUP -> {}", e.getMessage());
+			;
+		}
+
+		return partida;
+	}
 }
